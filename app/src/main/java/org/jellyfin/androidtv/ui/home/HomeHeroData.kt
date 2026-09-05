ec291242e3e1092b96e75f78bf86879c4c34d042
Mohd Fahad Khan <darushkhan786@gmail.com>
2026-09-05 06:53:41 +0530

    DDFlix 0004: add hero data model

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/home/HomeHeroData.kt
@@ -0,0 +1,8 @@
+package org.jellyfin.androidtv.ui.home
+
+import org.jellyfin.sdk.model.api.BaseItemDto
+
+/** Simple holder for hero presentation data. */
+data class HomeHeroData(
+	val item: BaseItemDto,
+)
