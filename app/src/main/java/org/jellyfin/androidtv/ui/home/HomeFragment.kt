7ca8bea6fe386a9dd5db58e5df4e9208e974c778
Mohd Fahad Khan <darushkhan786@gmail.com>
2026-09-05 06:52:27 +0530

    DDFlix 0004: keep HomeFragment identical to main

--- app/src/main/java/org/jellyfin/androidtv/ui/home/HomeFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/home/HomeFragment.kt
@@ -51,6 +51,9 @@ class HomeFragment : Fragment() {
 			Column {
 				MainToolbar(MainToolbarActiveButton.Home)
 
+				// The leanback code has its own awful focus handling that doesn't work properly with Compose view inteop to workaround this
+				// issue we add custom behavior that only allows focus exit when the current selected row is the first one. Additionally when
+				// we do switch the focus, we reset the leanback state so it won't cause weird behavior when focus is regained
 				var rowsSupportFragment by remember { mutableStateOf<HomeRowsFragment?>(null) }
 				AndroidFragment<HomeRowsFragment>(
 					modifier = Modifier
@@ -68,7 +71,9 @@ class HomeFragment : Fragment() {
 							}
 						}
 						.fillMaxSize(),
-					onUpdate = { fragment -> rowsSupportFragment = fragment },
+					onUpdate = { fragment ->
+						rowsSupportFragment = fragment
+					}
 				)
 			}
 		}
@@ -79,8 +84,13 @@ class HomeFragment : Fragment() {
 
 		sessionRepository.currentSession
 			.flowWithLifecycle(viewLifecycleOwner.lifecycle, Lifecycle.State.STARTED)
-			.map { session -> if (session == null) null else serverRepository.getServer(session.serverId) }
-			.onEach { server -> notificationRepository.updateServerNotifications(server) }
+			.map { session ->
+				if (session == null) null
+				else serverRepository.getServer(session.serverId)
+			}
+			.onEach { server ->
+				notificationRepository.updateServerNotifications(server)
+			}
 			.launchIn(viewLifecycleOwner.lifecycleScope)
 	}
 }
